199795d698cffd1c31545b456fd6de5b147204fe
Daniel Diaz <1813011317@untels.edu.pe>
2023-07-03 17:15:29 -0500

    Daniel Diaz

---
+++ app/src/main/java/com/example/damsem10_recyclerview/NotasActivity.java
@@ -0,0 +1,14 @@
+package com.example.damsem10_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+    }
+}
